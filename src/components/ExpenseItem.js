26a201ed01569fd0d513ab3eeb6e16c2a8e94cc7
befierce <enterbhatt123@gmail.com>
2023-11-07 12:30:32 +0530

    added a function click handler to editexpense button

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,6 +2,11 @@ import "./ExpenseItem.css";
 import ExpenseDate from './ExpenseDate';
 
 function ExpenseItem(props) {
+  const clickHandler =()=>{
+    console.log('clicked!!!!!!!')
+  }
+
+
   return (
     <div className="expense-item">
       <ExpenseDate date = {props.date}/>
@@ -9,6 +14,7 @@ function ExpenseItem(props) {
         <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount}</div>
       </div>
+      <button onClick={clickHandler}>editItem</button>
     </div>
   );
 }
