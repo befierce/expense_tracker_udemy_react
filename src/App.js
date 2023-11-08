740dd297355d94863548e9f9e4c35558f62b53e5
befierce <enterbhatt123@gmail.com>
2023-11-08 11:13:19 +0530

    made form to inpput expenses using react components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 // import logo from './logo.svg';
-import ExpenseItem from "./components/ExpenseItem";
+import ExpenseItem from "./components/Expenses/ExpenseItem";
+import NewExpense from "./components/newExpense/NewExpense"
+
 import "./App.css";
 
 function App() {
@@ -26,18 +28,18 @@ function App() {
   ];
   return (
     <div>
-      <h2>Let's get started</h2>
+      <NewExpense />
       <ExpenseItem
         title={expenses[0].title}
         amount={expenses[0].amount}
         date={expenses[0].date}
       ></ExpenseItem>
-     <ExpenseItem
+      <ExpenseItem
         title={expenses[1].title}
         amount={expenses[1].amount}
         date={expenses[1].date}
       ></ExpenseItem>
-       <ExpenseItem
+      <ExpenseItem
         title={expenses[2].title}
         amount={expenses[2].amount}
         date={expenses[2].date}
@@ -52,32 +54,3 @@ function App() {
 }
 
 export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
